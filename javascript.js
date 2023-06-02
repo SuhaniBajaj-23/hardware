042784637a4c49af4c9051a49b2c19926fbd5ff1
SuhaniBajaj-23 <suhani.bajaj2020@vitstudent.ac.in>
2023-06-02 12:36:24 +0530

    some changes

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@ const container = document.querySelector(".container"),
 
       signUp.addEventListener("click", ( )=>{
         container.classList.add("active");
-    });
-    login.addEventListener("click", ( )=>{
-        container.classList.remove("active");
-    });
+      });
+      login.addEventListener("click", ( )=>{
+          container.classList.remove("active");
+      });
 
 
 function signup(event) {
@@ -18,29 +18,20 @@ function signup(event) {
     var contact = document.querySelector('#ContactNo').value;
     var pass = document.querySelector('#password').value;
     var cpass = document.querySelector('#Cpassword').value;
-    var lowerCaseLetters = /[a-z]/g;
-    var upperCaseLetters = /[A-Z]/g;
-    var numbers = /[0-9]/g;
-  
-    if (name.length === 0) {
-      alert('Please Enter Full Name');
-    } else if (email.length === 0) {
-      alert('Please fill in Email');
-    } else if (contact.length === 0) {
-      alert('Please fill in Contact');
-    } else if (pass.length === 0) {
-      alert('Please fill in password');
-    } else if (cpass.length === 0) {
-      alert('Please fill in confirm password');
-    } else if (!pass.match(numbers)) {
-      alert('please add 1 number');
-    } else if (!pass.match(upperCaseLetters)) {
-      alert('please add 1 uppercase letter');
-    } else if (!pass.match(lowerCaseLetters)) {
-      alert('please add 1 lowercase letter');
-    } else if (pass.length < 6) {
-      alert('Password length should not be less than 6');
-    } else if (pass !== cpass) {
+    var passwordStrength = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
+    // var lowerCaseLetters = /[a-z]/g;
+    // var upperCaseLetters = /[A-Z]/g;
+    // var numbers = /[0-9]/g;
+    // if (!pass.match(numbers)) {
+    //   alert('please add 1 number');
+    // } else if (!pass.match(upperCaseLetters)) {
+    //   alert('please add 1 uppercase letter');
+    // } else if (!pass.match(lowerCaseLetters)) {
+    //   alert('please add 1 lowercase letter');
+    // }
+    if(!passwordStrength.test(pass)){
+      alert('Password should contain 1 uppercase letter, 1 lowercase letter, 1 numerical, 1 special character and should be atleast 8 characters long');
+    }else if (pass !== cpass) {
       alert('please enter the same password in confirm password');
     } else if (contact.length !== 10) {
       alert('enter correct contact no');
@@ -52,13 +43,16 @@ function signup(event) {
         password: pass,
         cpassword: cpass
       };
+      //get local storage in the form of a string
       var usersr = JSON.parse(localStorage.getItem('Users')) || [];
+      //check if user exists in local storage
       if(!usersr.some(user => user.email === userData.email)) {
         usersr.push(userData);
         localStorage.setItem('Users', JSON.stringify(usersr));
-        location.reload()
+        // location.reload()
         console.log(userData)
         alert("successfully signed in");
+        container.classList.remove("active");
        }
        else {
         alert("user already exits");
@@ -75,11 +69,11 @@ function signup(event) {
 
     if (user) {
         if (user.password === password) {
-          console.log("Valid credentials");
+          alert("Valid credentials");
         } else {
-          console.log("Invalid credentials");
+          alert("Invalid credentials");
         }
       } else {
-        console.log("User does not exist");
+        alert("User does not exist");
       }
 }
